4ed3b87292a1005f0da3867a77e8fb5428d42058
kelshardy <14024@coderacademy.edu.au>
2023-10-13 13:43:40 +1100

    fix: food counter reveals final meal

--- src/components/FoodOption.jsx
+++ src/components/FoodOption.jsx
@@ -1,10 +1,35 @@
-export default function FoodOption({ name, url, handleClick }) {
-    return (
-      <>
-        <h2>{name}</h2>
-        <button  className="btn btn-select" type="button" onClick={handleClick}>✓</button>
-        <img src={url} alt={name} style={{ maxHeight: "300px" }} />
-        
-      </>
-    );
-  }
+import { Col, Row } from "react-bootstrap";
+
+export default function FoodOption({ name, url, handleClick, hideButton, recipe }) {
+  return (
+    <>
+      <h2>{name}</h2>
+      {/* Hides the tick button for Food Options when the winning meal is chosen */}
+      {hideButton === true ? (
+        <></>
+      ) : (
+        <>
+          <button
+            className="btn btn-select"
+            type="button"
+            onClick={handleClick}
+          >
+            ✓
+          </button>
+        </>
+      )}
+      <Row>
+        {/* Centres the image for the food comparison and also the final meal */}
+        <Col md={{ span: 6, offset: 3 }}>
+          <img src={url} alt={name} style={{ maxHeight: "300px" }} />
+        </Col>
+      </Row>
+      {/* Recipe is truthy when final meal is chosen, therefore it is displayed */}
+      {recipe ? (
+        <p>{recipe}</p>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+}
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -16,12 +16,18 @@ function HomePage() {
 	const [meal1Recipe, setMeal1Recipe] = useState("");
 	const [meal2Recipe, setMeal2Recipe] = useState("");
 	const [mealsSelected, setMealsSelected] = useState(0);
+	const [mealWinnerName, setMealWinnerName] = useState(null);
+	const [mealWinnerImage, setMealWinnerImage] = useState(null);
+	const [mealWinnerRecipe, setMealWinnerRecipe] = useState(null);
 
 	// Function to handle the "Start" button click
 	const handleStartClick = async () => {
 		try {
 			// On Start Click count is reset to 0.
 			setMealsSelected(0);
+			setMealWinnerName(null);
+			setMealWinnerImage(null);
+			setMealWinnerRecipe(null);
 			// API requests to get random meal images
 			const response1 = await axios.get(
 				"https://www.themealdb.com/api/json/v1/1/random.php"
@@ -44,7 +50,7 @@ function HomePage() {
 			const meal1Recipe = response1.data.meals[0]?.strInstructions;
 			const meal2Recipe = response2.data.meals[0]?.strInstructions;
 
-			// Update state to display the fetched images
+			// Update state to display the fetched images, names and recipes
 			setMeal1Image(meal1ImageUrl);
 			setMeal1Name(meal1NameUrl);
 			setMeal2Image(meal2ImageUrl);
@@ -65,25 +71,37 @@ function HomePage() {
 
 			// Retrieve Image from Meal
 			const mealImageUrl = meal.data.meals[0]?.strMealThumb;
-			// Retrieve Image from Meal
+			// Retrieve Name from Meal
 			const mealNameUrl = meal.data.meals[0]?.strMeal;
-
+			// Retrieve Recipe from Meal
+			const mealRecipe = meal.data.meals[0]?.strInstructions;
+			
+			// If Meal #1 is clicked, Meal #2 is reshuffled
 			if (mealId === 1) {
 				setMeal2Image(mealImageUrl);
 				setMeal2Name(mealNameUrl);
+				setMeal2Recipe(mealRecipe);
+			// If Meal #2 is clicked, Meal #1 is reshuffled
 			} else if (mealId === 2) {
 				setMeal1Image(mealImageUrl);
 				setMeal1Name(mealNameUrl);
+				setMeal1Recipe(mealRecipe);
 			}
-
+			// Increases the meal count as each click occurs
 			setMealsSelected(mealsSelected + 1);
-			// if mealsSelected > 3: display recipe for last selection
-			if (mealsSelected >= 3) {
-				let thisGuy = meal1Name;
+			// If mealsSelected reaches 5: display recipe for last selection
+			if (mealsSelected >= 4) {
+				// If Meal #2 is selected, meal information is set as Winner state
 				if (mealId === 2) {
-					thisGuy = meal2Name;
+					setMealWinnerName(meal2Name);
+					setMealWinnerImage(meal2Image);
+					setMealWinnerRecipe(meal2Recipe);
+				// If Meal #1 is selected, meal information is set as Winner state
+				} else if (mealId === 1) {
+					setMealWinnerName(meal1Name);
+					setMealWinnerImage(meal1Image);
+					setMealWinnerRecipe(meal1Recipe);
 				}
-				alert(`Looks like you're having ${thisGuy} for dinner`);
 			}
 		} catch (error) {
 			console.error("Error fetching meal image:", error);
@@ -96,34 +114,47 @@ function HomePage() {
 			<button className="btn btn-primary" onClick={handleStartClick}>
 				Start
 			</button>
-			<h3>Choices: {mealsSelected}</h3>
-			<Row>
-				{/* Display the first meal's name and image */}
-				<Col md={6} className="mb-3">
-					<FoodOption
-						name={meal1Name}
-						url={meal1Image}
-						handleClick={() => handleMealClick(1)}
-					/>
-				</Col>
+			{/* No Winner is set from Start, so FoodOptions are displayed */}
+			{mealWinnerName === null ? (
+				<>
+					<Row>
+						{/* Display the first meal's name and image */}
+						<Col md={6} className="mb-3">
+							<FoodOption
+								name={meal1Name}
+								url={meal1Image}
+								handleClick={() => handleMealClick(1)}
+								/>
+						</Col>
 
-				{/* Display the second meal's name and image */}
-				<Col md={6} className="mb-3">
-					<FoodOption
-						name={meal2Name}
-						url={meal2Image}
-						handleClick={() => handleMealClick(2)}
+						{/* Display the second meal's name and image */}
+						<Col md={6} className="mb-3">
+							<FoodOption
+								name={meal2Name}
+								url={meal2Image}
+								handleClick={() => handleMealClick(2)}
+							/>
+						</Col>
+					</Row>
+					{/* Include the Recipe component with meal data */}
+					<Recipe
+						meal1Name={meal1Name}
+						meal2Name={meal2Name}
+						meal1Image={meal1Image}
+						meal2Image={meal2Image}
 					/>
-				</Col>
-			</Row>
-
-			{/* Include the Recipe component with meal data */}
-			<Recipe
-				meal1Name={meal1Name}
-				meal2Name={meal2Name}
-				meal1Image={meal1Image}
-				meal2Image={meal2Image}
-			/>
+				</>
+			) : (
+				// When setWinner is not null, selected meal recipe is revealed
+				<>
+					<FoodOption
+						name={mealWinnerName}
+						hideButton={true}
+						url={mealWinnerImage}
+						recipe={mealWinnerRecipe}
+					/>
+				</>
+			)}
 		</Container>
 	);
 }
